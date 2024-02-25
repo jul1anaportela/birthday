65b4495f0098a306694d1d602b0b761f2bbcb6c3
Juliana Portela <bazil.helpdesk@plan-international.org>
2024-02-24 21:03:26 -0300

    atividade Módulo 22

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const dataDoEvento = new Date("Mar 28, 2024 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minEmMs = 1000 * 60;
+    const secEmMs = 1000
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minEmMs) / secEmMs)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}min ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+
+}, 1000)
